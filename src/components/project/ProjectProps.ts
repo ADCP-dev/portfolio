349eabb0df57564ff3a1a78a56a56b96f4487773
EtherTCode <adriancolom@paucasesnovescifp.cat>
2024-01-12 23:54:47 +0100

    Create Project cards and remove "show more button"

---
+++ src/components/project/ProjectProps.ts
@@ -0,0 +1,17 @@
+interface Technology {
+  name: string;
+  url: string;
+  img: string;
+}
+
+export interface ProjectProps {
+  title: string;
+  description: string;
+  technologies: Technology[];
+  dateStart: string;
+  dateEnd: string;
+  workingAt: boolean;
+  url: string;
+  img: string;
+  public: boolean;
+}
